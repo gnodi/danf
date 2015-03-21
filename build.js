a45803d7dbf74e876414da3b7993cca09cb6acda
Gnucki <tprelot@gmail.com>
2015-03-21 14:47:56 +0100

    Handle client commonjs conversion

--- build.js
+++ build.js
@@ -4,5 +4,13 @@
     out: 'main-built.js',
     optimize: 'none',
     absolutise: true,
-    cjsTranslate: true
+    cjsTranslate: true,
+    paths: {
+        'jquery': 'node_modules/jquery/dist/jquery'
+    },
+    map: {
+        // Use no conflict jQuery.
+        '*': { 'jquery': 'lib/client/vendor/jquery' },
+        'lib/client/vendor/jquery': { 'jquery': 'jquery' }
+    }
 })
--- config/client/vendor/classes.js
+++ config/client/vendor/classes.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = {
-    jquery: require('../../../lib/client/vendor/jquery'),
+    jquery: require('../../../lib/client/vendor/jquery')
 };
