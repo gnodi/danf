000bde9ad1bbe9d78236b9737877ddf344e0a668
Gnucki <tprelot@gmail.com>
2015-09-25 16:50:49 +0200

    Upgrade ajax app

--- config/client/ajax-app/sequences.js
+++ config/client/ajax-app/sequences.js
@@ -1,50 +1,74 @@
 'use strict';
 
 module.exports = {
-    process: [
-        {
-            service: 'danf:ajaxApp.historyHandler',
-            method: 'initialize',
-            arguments: []
-        }
-    ],
-    processAjax: [
-        {
-            service: 'danf:event.notifier.dom',
-            method: 'refreshListeners',
-            arguments: ['@data@']
-        }
-    ],
-    followLink: [
-        {
-            service: 'danf:ajaxApp.linkFollower',
-            method: 'follow',
-            arguments: ['!event.currentTarget!']
+    process: {
+        data: {
+            scope: {
+                type: 'object',
+                default: null
+            }
         },
-        {
-            service: 'danf:manipulation.history',
-            method: 'replace'
-        }
-    ],
-    submitForm: [
-        {
-            service: 'danf:ajaxApp.formSubmitter',
-            method: 'submit',
-            arguments: ['!event.currentTarget!']
-        },
-        {
-            condition: function(stream, context) {
-
+        operations: [
+            {
+                service: 'danf:ajaxApp.historyHandler',
+                method: 'initialize',
+                arguments: ['@scope@']
+            }
+        ]
+    },
+    processAjax: {
+        operations: [
+            {
+                service: 'danf:event.notifier.dom',
+                method: 'refreshListeners',
+                arguments: ['@data@']
+            }
+        ]
+    },
+    followLink: {
+        operations: [
+            {
+                service: 'danf:ajaxApp.linkFollower',
+                method: 'follow',
+                arguments: ['!event.currentTarget!'],
+                scope: 'content'
             },
-            service: 'danf:manipulation.history',
-            method: 'replace'
-        }
-    ],
-    navigate: [
-        {
-            service: 'danf:ajaxApp.historyHandler',
-            method: 'navigate',
-            arguments: ['!event.originalEvent.state!']
-        }
-    ]
+            {
+                service: 'danf:ajaxApp.linkFollower',
+                method: 'write',
+                arguments: ['@content@']
+            },
+            {
+                service: 'danf:manipulation.history',
+                method: 'replace'
+            }
+        ]
+    },
+    submitForm: {
+        operations: [
+            {
+                service: 'danf:ajaxApp.formSubmitter',
+                method: 'submit',
+                arguments: ['!event.currentTarget!']
+            },
+            {
+                service: 'danf:ajaxApp.formSubmitter',
+                method: 'write',
+                arguments: ['@content@', '!event.currentTarget!']
+            },
+            {
+                service: 'danf:manipulation.history',
+                method: 'replace'
+            }
+        ]
+    },
+    navigate: {
+        operations: [
+            {
+                service: 'danf:ajaxApp.historyHandler',
+                method: 'navigate',
+                arguments: ['!event.originalEvent.state!']
+            }
+        ]
+    },
 };
