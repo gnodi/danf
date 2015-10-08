83e96a5612a76c5400c175af58f3678e576794a4
Gnucki <tprelot@gmail.com>
2015-10-08 18:55:04 +0200

    Add event children for factorization

--- test/unit/event/configuration/section-processor/events.js
+++ test/unit/event/configuration/section-processor/events.js
@@ -3,6 +3,7 @@
 require('../../../../../lib/common/init');
 
 var assert = require('assert'),
+    utils = require('../../../../../lib/common/utils'),
     Events = require('../../../../../lib/common/event/configuration/section-processor/events')
 ;
 
@@ -31,87 +32,114 @@ events.addNotifier(requestNotifier);
 
 describe('Events section configuration processor', function() {
     it('should aggregate the contracts of notifiers', function() {
-        assert.deepEqual(
-            events.contract,
-            {
-                event: {
-                    type: 'embedded_object',
-                    namespace: true,
-                    references: ['$'],
-                    embed: {
-                        event: {type: 'string'},
-                        sequences: {
-                            type: 'embedded_array',
-                            embed: {
-                                name: {
-                                    type: 'string',
-                                    required: true,
-                                    namespace: true
-                                },
-                                condition: {
-                                    type: 'function'
-                                },
-                                order: {
-                                    type: 'number',
-                                    default: 0
-                                },
-                                collection: {
-                                    type: 'embedded',
-                                    embed: 'plop'
-                                },
-                                input: {
-                                    type: 'mixed_object',
-                                    default: {}
-                                },
-                                output: {
-                                    type: 'mixed_object',
-                                    default: {}
-                                }
+        var eventContract = {
+                type: 'embedded_object',
+                embed: {
+                    event: {type: 'string'},
+                    sequences: {
+                        type: 'embedded_array',
+                        embed: {
+                            name: {
+                                type: 'string',
+                                required: true,
+                                namespace: true
+                            },
+                            condition: {
+                                type: 'function'
+                            },
+                            order: {
+                                type: 'number',
+                                default: 0
+                            },
+                            collection: {
+                                type: 'embedded',
+                                embed: 'plop'
+                            },
+                            input: {
+                                type: 'mixed_object',
+                                default: {}
+                            },
+                            output: {
+                                type: 'mixed_object',
+                                default: {}
                             }
-                        },
-                        data: {type: 'object'}
-                    }
-                },
-                request: {
-                    type: 'embedded_object',
-                    namespace: true,
-                    references: ['$'],
-                    embed: {
-                        path: {type: 'string'},
-                        methods: {type: 'string_array'},
-                        sequences: {
-                            type: 'embedded_array',
-                            embed: {
-                                name: {
-                                    type: 'string',
-                                    required: true,
-                                    namespace: true
-                                },
-                                condition: {
-                                    type: 'function'
-                                },
-                                order: {
-                                    type: 'number',
-                                    default: 0
-                                },
-                                collection: {
-                                    type: 'embedded',
-                                    embed: 'plop'
-                                },
-                                input: {
-                                    type: 'mixed_object',
-                                    default: {}
-                                },
-                                output: {
-                                    type: 'mixed_object',
-                                    default: {}
-                                }
+                        }
+                    },
+                    data: {type: 'object'}
+                }
+            },
+            requestContract = {
+                type: 'embedded_object',
+                embed: {
+                    path: {type: 'string'},
+                    methods: {type: 'string_array'},
+                    sequences: {
+                        type: 'embedded_array',
+                        embed: {
+                            name: {
+                                type: 'string',
+                                required: true,
+                                namespace: true
+                            },
+                            condition: {
+                                type: 'function'
+                            },
+                            order: {
+                                type: 'number',
+                                default: 0
+                            },
+                            collection: {
+                                type: 'embedded',
+                                embed: 'plop'
+                            },
+                            input: {
+                                type: 'mixed_object',
+                                default: {}
+                            },
+                            output: {
+                                type: 'mixed_object',
+                                default: {}
                             }
-                        },
-                        data: {type: 'object'}
-                    }
+                        }
+                    },
+                    data: {type: 'object'}
                 }
             }
+        ;
+
+        var eventContract_ = eventContract.embed,
+            originalEventContract = utils.clone(eventContract),
+            requestContract_ = requestContract.embed,
+            originalRequestContract = utils.clone(requestContract)
+        ;
+
+        for (var i = 0; i <= 9; i++) {
+            eventContract_.children = utils.clone(originalEventContract);
+            eventContract_ = eventContract_.children.embed;
+            requestContract_.children = utils.clone(originalRequestContract);
+            requestContract_ = requestContract_.children.embed;
+        }
+
+        eventContract.namespace = true;
+        eventContract.references = ['$'];
+        eventContract.validate = '...';
+        requestContract.namespace = true;
+        requestContract.references = ['$'];
+        requestContract.validate = '...';
+
+        var expectedContract = {
+                event: eventContract,
+                request: requestContract
+            },
+            contract = events.contract
+        ;
+
+        contract.event.validate = '...';
+        contract.request.validate = '...';
+
+        assert.deepEqual(
+            contract,
+            expectedContract
         );
     })
 })
