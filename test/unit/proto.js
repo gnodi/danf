2f91df9504c28c8781d708c106d6e2b37e4ee052
Gnucki <tprelot@gmail.com>
2015-10-24 16:45:13 +0200

    Add configuration auto building

--- test/unit/proto.js
+++ test/unit/proto.js
@@ -5,18 +5,73 @@ var assert = require('assert'),
     danf = require('../../lib/server/app')
 ;
 
-var rootPath = path.join(_dirname, '/../fixture/proto');
+var rootPath = path.join(__dirname, '/../fixture/proto');
 
 describe('Danf proto application', function() {
     it('should build its configuration from files, folders and node modules', function() {
         assert.deepEqual(
             danf.prototype.buildServerConfiguration(rootPath, 'danf'),
-            {}
+            {
+                config: {
+                    classes: {}
+                },
+                dependencies: {
+                    a: {
+                        config: {
+                            events: {
+                                request: {
+                                    compute: {},
+                                    api: {
+                                        children: {
+                                            user: {
+                                                children: {
+                                                    message: {}
+                                                }
+                                            },
+                                            oldTopic: {},
+                                            topic: {}
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                        dependencies: {
+                            c: {
+                                contract: {},
+                                dependencies: {}
+                            }
+                        }
+                    }
+                }
+            }
         );
 
         assert.deepEqual(
             danf.prototype.buildSpecificConfiguration(rootPath, 'danf', 'client'),
-            {}
+            {
+                config: {
+                    classes: {}
+                },
+                dependencies: {
+                    a: {
+                        config: {
+                            this: {},
+                            events: {
+                                request: {
+                                    compute: {}
+                                }
+                            }
+                        },
+                        contract: {},
+                        dependencies: {
+                            b: {
+                                contract: {},
+                                dependencies: {}
+                            }
+                        }
+                    }
+                }
+            }
         );
     })
-}
+})
