fa8e531e1ecc43b8538615e61925dff1ce473b25
Gnucki <tprelot@gmail.com>
2015-10-26 18:17:00 +0100

    Add client object config test

--- test/functional/feature/ajax-hello-world/app.js
+++ test/functional/feature/ajax-hello-world/app.js
@@ -2,7 +2,71 @@
 
 var danf = require('../../../..'),
     app = danf(
+        // SERVER.
+        {
+            config: {
+                events: {
+                    request: {
+                        helloWorld: {
+                            path: '',
+                            methods: ['get'],
+                            view: {
+                                html: {
+                                    body: {
+                                        file: __dirname + '/index.jade'
+                                    }
+                                }
+                            },
+                        },
+                        hello: {
+                            path: '/hello',
+                            methods: ['get'],
+                            view: {
+                                html: {
+                                    body: {
+                                        file: __dirname + '/hello.jade'
+                                    }
+                                }
+                            },
+                        },
+                        world: {
+                            path: '/world',
+                            methods: ['get'],
+                            view: {
+                                html: {
+                                    body: {
+                                        file: __dirname + '/world.jade'
+                                    }
+                                }
+                            },
+                        }
+                    }
+                }
+            }
+        },
+        // CLIENT.
+        {
+            config: {
+                events: {
+                    request: {
+                        hello: {
+                            path: '/hello',
+                            methods: ['get']
+                        },
+                        world: {
+                            path: '/world',
+                            methods: ['get']
+                        }
+                    }
+                }
+            }
+        }
+    )
+;
+
+/*var danf = require('../../../..'),
+    app = danf(
         __dirname + '/danf-server.js',
         __dirname + '/danf-client.js'
     )
-;
+;*/
