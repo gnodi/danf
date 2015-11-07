0ce398ba1cd3ccdcfe6602a4538fd9ff14daefba
Gnucki <tprelot@gmail.com>
2015-11-07 17:13:04 +0100

    Remove deprecated tutorial

--- test/functional/proto/tutorial/config/client/events/dom.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-    formSubmitting: {
-        selector: '#framework-form :submit',
-        event: 'click',
-        sequences: ['startComputingMeasure']
-    }
-};
