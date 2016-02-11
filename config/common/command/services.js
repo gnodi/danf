8ea142105ceaa9031d4f35a741825177a502eb13
Thomas Prelot <tprelot@gmail.com>
2016-02-11 14:00:52 +0100

    Upgrade debuggability

--- config/common/command/services.js
+++ config/common/command/services.js
@@ -20,7 +20,10 @@ module.exports = {
                 parent: 'danf:event.notifier',
                 children: {
                     request: {
-                        class: 'danf:command.event.notifier.command'
+                        class: 'danf:command.event.notifier.command',
+                        properties: {
+                            logger: 'danf:logging.logger'
+                        }
                     }
                 }
             }
