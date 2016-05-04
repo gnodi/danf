2c43c2afaba00dfd2277f0244b7eb0c1af6fbdac
Gnucki <tprelot@gmail.com>
2016-05-04 21:51:35 +0200

    Upgrade ajax app

--- config/client/ajax-app/sequences.js
+++ config/client/ajax-app/sequences.js
@@ -11,13 +11,27 @@ module.exports = {
                 scope: 'response'
             },
             {
+                order: 1,
+                service: 'danf:manipulation.callbackExecutor',
+                method: 'execute',
+                arguments: [
+                    function(response) {
+                        var url = response.headers['X-Danf-Path'];
+
+                        return url ? url : response.url;
+                    },
+                    '@response@'
+                ],
+                scope: 'url'
+            },
+            {
                 condition: function(stream) {
                     return stream.response && stream.response.status < 400;
                 },
                 order: 10,
                 service: 'danf:ajaxApp.linkFollower',
                 method: 'write',
-                arguments: ['@response.text@', '@response.url@', '!event!']
+                arguments: ['@response.text@', '@url@', '!event!']
             }
         ]
     },
@@ -31,13 +45,27 @@ module.exports = {
                 scope: 'response'
             },
             {
+                order: 1,
+                service: 'danf:manipulation.callbackExecutor',
+                method: 'execute',
+                arguments: [
+                    function(response) {
+                        var url = response.headers['X-Danf-Path'];
+
+                        return url ? url : response.url;
+                    },
+                    '@response@'
+                ],
+                scope: 'url'
+            },
+            {
                 condition: function(stream) {
                     return stream.response && stream.response.status < 400;
                 },
                 order: 10,
                 service: 'danf:ajaxApp.formSubmitter',
                 method: 'write',
-                arguments: ['@response.text@', '@response.url@', '!event!']
+                arguments: ['@response.text@', '@url@', '!event!']
             }
         ]
     }
